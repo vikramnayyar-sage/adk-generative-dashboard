0572af9ff5cf8ebb7fb468a9e64422fdf364a3ea
vikramnayyar-sage <vikram.nayyar@sage.com>
2025-10-04 20:35:20 +0100

    Configure MCP connection over STDIO

--- agent/agent.py
+++ agent/agent.py
@@ -6,6 +6,9 @@ from google.adk.agents import Agent
 from google.adk.agents.callback_context import CallbackContext
 from google.adk.tools import google_search, FunctionTool
 from google.adk.tools.agent_tool import AgentTool
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import SseConnectionParams, StdioConnectionParams, StdioServerParameters
+from fastmcp import FastMCP
 
 
 
@@ -20,19 +23,41 @@ def on_before_agent(callback_context: CallbackContext):
   """
   return None
 
-search_agent = Agent(
-    model='gemini-2.0-flash',
-    name='SearchAgent',
-    instruction="""
-    You're a specialist in Google Search
-    """,
-    tools=[google_search],
+mcp_toolset = McpToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command="fastmcp",
+            args=["run", "main.py:mcp"],
+            cwd="c:/Dev/hackathon-prep/adk-generative-dashboard/agent/mcp",  # adjust to your actual MCP folder
+        )
+    )
 )
+
+print(f"TOOLS:")
+print(tools)
+print(f"MCP TOOLSET:")
+print(mcp_toolset)
+
+
+
+search_agent = Agent(
+  model='gemini-2.0-flash',
+  name='SearchAgent',
+  instruction="""
+  You're a specialist in Google Search
+  """,
+  tools=[
+    google_search,
+    mcp_toolset #FROM TEST MCP Server.
+  
+  ],
+)
+
 
 dashboard_agent = Agent(
   name="DashboardAgent",
   model="gemini-2.5-flash",
-  tools=tools + [AgentTool(agent=search_agent)],
+  tools=tools + [AgentTool(agent=search_agent), mcp_toolset],
 
   # run-loop modifiers
   before_agent_callback=on_before_agent,
--- agent/mcp/main.py
+++ agent/mcp/main.py
@@ -7,5 +7,13 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+@mcp.tool
+def special_greeting(name: str) -> str:
+    """
+    Provide a special greeting, using the user's name.
+    Always returns a plain string for compatibility with ADK/JSON serialization.
+    """
+    return f"Hello {name} this is a special greeting for you."
+
 if __name__ == "__main__":
     mcp.run()
