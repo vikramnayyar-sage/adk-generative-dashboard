ddaf5d7f18ad4ad66de36545b83b18eeb0c4c961
vikramnayyar-sage <vikram.nayyar@sage.com>
2025-10-08 14:09:24 +0100

    Add back in MCP support

--- agent/agent.py
+++ agent/agent.py
@@ -6,6 +6,9 @@ from google.adk.agents import Agent
 from google.adk.agents.callback_context import CallbackContext
 from google.adk.tools import google_search, FunctionTool
 from google.adk.tools.agent_tool import AgentTool
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import SseConnectionParams, StdioConnectionParams, StdioServerParameters
+from fastmcp import FastMCP
 
 
 
@@ -13,6 +16,7 @@ from google.adk.tools.agent_tool import AgentTool
 from modifiers import before_model, after_model
 from tools import tools
 from instructions import instruction_provider
+
 
 
 import os
@@ -50,21 +54,38 @@ def on_before_agent(callback_context: CallbackContext):
     callback_context.state['cashflowEntries'] = [entry.dict() for entry in mock_entries]
   return None
 
-# Commenting out SearchAgent to improve performance - not needed for dashboard
-# search_agent = Agent(
-#     model='gemini-2.0-flash',
-#     name='SearchAgent',
-#     instruction="""
-#     You're a specialist in Google Search
-#     """,
-#     tools=[google_search],
-# )
+
+mcp_toolset = McpToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command="fastmcp",
+            args=["run", "main.py:mcp"],
+            cwd="c:/Dev/hackathon-prep/adk-generative-dashboard/agent/mcp",
+        )
+    )
+)
+
+print(f"TOOLS:")
+print(tools)
+print(f"MCP TOOLSET:")
+print(mcp_toolset)
+
+search_agent = Agent(
+  model='gemini-2.0-flash',
+  name='SearchAgent',
+  instruction="""
+  You're a specialist in Google Search
+  """,
+  tools=[
+    google_search,
+    mcp_toolset #FROM TEST MCP Server.
+  ],
+)
 
 dashboard_agent = Agent(
   name="DashboardAgent",
   model="gemini-2.5-flash",
-  tools=tools,  # Removed SearchAgent for better performance
-  # tools=tools + [AgentTool(agent=search_agent)],  # Original with SearchAgent
+  tools=tools + [AgentTool(agent=search_agent), mcp_toolset],
 
   # run-loop modifiers
   before_agent_callback=on_before_agent,
